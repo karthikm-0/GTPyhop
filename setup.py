d872247e37a86bd6b95043b0ccb08342f2834367
karthik <karthikm@dgp.toronto.edu>
2024-02-07 15:58:44 -0500

    Made pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gtpyhop',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple planner module',
+    author='Dana S. Nau',
+    author_email='nau@umd.edu',
+    url='https://github.com/dananau/GTPyhop',
+)
